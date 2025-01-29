5e65f934e4efd6af61f361b219cdf015a3886d3e
StevenRVE <steven_ve@live.nl>
2025-01-29 21:54:01 +0100

    Updated Hero to be adaptive to desktop and mobile layout

--- sve-portfolio-website/src/component/layout/Hero.tsx
+++ sve-portfolio-website/src/component/layout/Hero.tsx
@@ -9,21 +9,21 @@ function Hero() {
       id="Hero"
       className="flex items-center min-h-screen px-20 bg-gradient-to-r from-gradientLeft to-gradientRight"
     >
-      <div className="flex flex-row items-center w-full">
-        <div className="flex flex-col z-20 pointer-events-none text-white w-3/5">
-          <p className="text-[5vw] font-bold text-lightcyan w-full">
+      <div className="flex flex-row items-center w-full desktop:flex-row desktop:items-center mobile:flex-col-reverse">
+        <div className="flex flex-col z-20 pointer-events-none text-white w-3/5 desktop:w-3/5 mobile:w-full mobile:text-center">
+          <p className="desktop:text-[5vw] mobile:text-[8vw] font-bold text-lightcyan w-full">
             Hello, I'm Steven
           </p>
-          <p className="text-[3vw] font-bold text-white w-full">
+          <p className="desktop:text-[3vw] mobile:text-[5vw]">
             a developer specializing in audio software development with
             <Typing />
           </p>
-          <div className="flex-mt-6 justify-start w-[17vw]">
+          <div className="flex mobile:hidden mt-6 justify-start w-[17vw]">
             <SocialMedia size={200} />
           </div>
         </div>
         <img
-          className="w-2/5 object-contain self-center animate-scalepulse"
+          className="w-2/5 mobile:w-full mobile:max-w-xl object-contain self-center animate-scalepulse"
           src="/images/portrait-steven-no-bg.webp"
           alt="Portrait"
         />
