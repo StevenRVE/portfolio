eb2be81b9a452394b29569d3677d5d12171e78fb
StevenRVE <steven_ve@live.nl>
2025-01-29 15:09:47 +0100

    Added burgermenu and moved MenuItem.tsx to src/components/menu/

---
+++ sve-portfolio-website/src/component/menu/elements/Close.tsx
@@ -0,0 +1,10 @@
+const Close = () => {
+  return (
+    <button className="relative z-50 p-6 bg-slate-200 bg-opacity-50 rounded-md">
+      <div className="w-8 h-1 rotate-45 -translate-x-1/2 absolute bg-white absolute-pulse"></div>
+      <div className="w-8 h-1 -rotate-45 -translate-x-1/2 absolute bg-white absolute-pulse"></div>
+    </button>
+  );
+};
+
+export default Close;
---
+++ sve-portfolio-website/src/component/menu/elements/Menu.tsx
@@ -0,0 +1,12 @@
+import MenuItem from "./MenuItem";
+
+const Menu = () => {
+  return (
+    <>
+      <MenuItem title={"About"} />
+      <MenuItem title={"Projects"} />
+      <MenuItem title={"Services"} />
+    </>
+  );
+};
+export default Menu;
