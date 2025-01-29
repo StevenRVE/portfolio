3833eb2b47ac9772fa315ac15d8b6ec091244c8d
StevenRVE <steven_ve@live.nl>
2025-01-29 21:55:54 +0100

    Implemented filler services with icons in Services.tsx

--- sve-portfolio-website/src/component/layout/Services.tsx
+++ sve-portfolio-website/src/component/layout/Services.tsx
@@ -1,3 +1,5 @@
+import Service from "@/static/Service";
+
 function Services() {
   return (
     <div className="bg-slate-800">
@@ -6,11 +8,25 @@ function Services() {
         src="/transitions/transition_gradient.svg"
         alt="transition"
       />
-      <div
-        id="Services"
-        className="flex justify-center items-center h-screen text-white"
-      >
+      <p className="flex text-4xl my-8 justify-center text-white font-bold ">
         Services
+      </p>
+      <div className="flex desktop:flex-row mobile:flex-col mobile:items-center flex-grow justify-around text-center">
+        <Service
+          headline="Service 1"
+          description="Service 1 description"
+          image="/icons/dashboard.svg"
+        />
+        <Service
+          headline="Service 2"
+          description="Service 2 description"
+          image="/icons/music-equalizer.svg"
+        />
+        <Service
+          headline="Service 3"
+          description="Service 3 description"
+          image="/icons/desktop-code.svg"
+        />
       </div>
     </div>
   );
