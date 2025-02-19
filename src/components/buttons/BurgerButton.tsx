93183c0cfbcdc01c9efc5e2f5fdd646d734d973b
StevenRVE <steven_ve@live.nl>
2025-02-19 23:30:19 +0100

    [refactor] changed Burger name to BurgerButton, onClick now part of BurgerButton

--- src/components/buttons/BurgerButton.tsx
+++ src/components/buttons/BurgerButton.tsx
@@ -1,8 +1,9 @@
-const Burger = () => {
+const BurgerButton = ({ onClick }: { onClick: () => void }) => {
   return (
     <button
       aria-label="Burger Menu Button"
       className="relative p-6 bg-slate-200 bg-opacity-50 rounded-md"
+      onClick={onClick}
     >
       <div className="absolute space-y-2 z-50 -translate-x-1/2 -translate-y-1/2">
         <div className="w-8 h-1 bg-white animate-pulse"></div>
@@ -13,4 +14,4 @@ const Burger = () => {
   );
 };
 
-export default Burger;
+export default BurgerButton;
