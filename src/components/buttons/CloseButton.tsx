393407789cd7d1134fb6198423f65a0f973bb29b
StevenRVE <steven_ve@live.nl>
2025-02-19 23:30:58 +0100

    [refacter] changed name Close to CloseButton, onClick now part of CloseButton

--- src/components/buttons/CloseButton.tsx
+++ src/components/buttons/CloseButton.tsx
@@ -1,10 +1,13 @@
-const Close = () => {
+const CloseButton = ({ onClick }: { onClick: () => void }) => {
   return (
-    <button className="relative z-50 p-6 bg-slate-200 bg-opacity-50 rounded-md">
+    <button
+      className="relative z-50 p-6 bg-slate-200 bg-opacity-50 rounded-md"
+      onClick={onClick}
+    >
       <div className="w-8 h-1 rotate-45 -translate-x-1/2 absolute bg-white absolute-pulse"></div>
       <div className="w-8 h-1 -rotate-45 -translate-x-1/2 absolute bg-white absolute-pulse"></div>
     </button>
   );
 };
 
-export default Close;
+export default CloseButton;
